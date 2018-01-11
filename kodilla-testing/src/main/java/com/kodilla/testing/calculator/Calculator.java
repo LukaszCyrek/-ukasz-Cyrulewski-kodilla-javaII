916d05601826842b0178c74959a666bb1fdffc31
LukaszCyrek <lukaszcyrek@o2.pl>
2018-01-11 19:45:29 +0100

    First Commit

---
+++ kodilla-testing/src/main/java/com/kodilla/testing/calculator/Calculator.java
@@ -0,0 +1,19 @@
+package com.kodilla.testing.calculator;
+
+public class Calculator {
+    int sum;
+    int minus;
+
+    public int add(int a, int b) {
+        sum = a + b;
+        return sum;
+    }
+
+    public int subtract(int c, int d) {
+        minus = c - d;
+        return minus;
+    }
+}
+
+
+
